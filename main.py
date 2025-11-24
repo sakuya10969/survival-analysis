53b27797742f3799dd19572da7d6634a3ddbd7ee
sakuya10969 <sakuya.f20010618@gmail.com>
2025-11-24 15:56:43 +0900

    feat: workflows

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from survival-analysis!")
+from src.load_data import load_lung_data
+from src.analysis.km import run_km
 
+def main():
+    df = load_lung_data()
+    run_km(df)
 
 if __name__ == "__main__":
     main()
